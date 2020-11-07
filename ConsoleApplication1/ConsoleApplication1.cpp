009be895eb8f26a37ec378c19d15c075af26aa04
Ilnur <https://github.com/ilnur2242/StudentsProgram.git>
2020-11-07 13:15:56 +0500

    7.11.20 - finish base part

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -84,19 +84,22 @@ public:
 		float y1_ = b.get()[1] - a.get()[1];
 
 		float d_inf = (y1_ * x2_ - y2_ * x1_);
-		float d_;
+		//float d_;
 
 		if (d_inf == 0) {
 			if ((a.get()[1] * x1_ - a.get()[0] * y1_) * x2_ == (l1.a.get()[1] * x2_ - l1.a.get()[0] * y2_) * x1_)
 			{
+				cout << " Lines are equal " << endl;
 				return 1; // equal
 			}
 			else
 			{
+				cout << " Lines are parallel " << endl;
 				return 2; // parallel
 			}
 		}
 		else {
+			cout << " Lines are intesection " << endl;
 			return 0;
 		}
 		//try
@@ -155,7 +158,7 @@ public:
 		int code = IsIntersecLine(l1);
 		switch (code)
 		{
-			case 0:
+			case 0: // intersrc
 			{
 				float d_ = x1_ * x2_ / d_inf;
 				x = d_ * ((-l1.a.get()[0] * y2_ + l1.a.get()[1] * x2_) / x2_ - (-a.get()[0] * y1_ + a.get()[1] * x1_) / x1_);
@@ -170,11 +173,19 @@ public:
 					return false;
 				}
 			}
-			case 1:
+			case 1: // equal
 			{
 				
+				float a10 = a.get()[0], a11 = a.get()[1]; 
+				float b10 = b.get()[0], b11 = b.get()[1];
+				float a20 = l1.a.get()[0], a21 = l1.a.get()[1];
+				float b20 = l1.b.get()[0], b21 = l1.b.get()[1];
+				// 
+				if ((a10 <= a20 && a20 <= b10 && a11 <= a21 && a21 <= b11) || (a10 <= b20 && b20 <= b10 && a11 <= b21 && b21 <= b11)) { return true; }
+				else if ((a20 <= a10 && a10 <= b20 && a21 <= a11 && a11 <= b21) || (a20 <= b10 && b10 <= b20 && a21 <= b11 && b11 <= b21)) { return true; }
+				else return false;
 			}
-			case 2:
+			case 2: // parallel
 				return false;
 		}
 		
@@ -186,14 +197,17 @@ int main()
 {
 
 	Point a(0, 0);
-	Point b(5, 5);
+	Point b(1, 0);
 
-	Point c(5, 5);
-	Point d(5, 6);
+	Point c(2,0);
+	Point d(4, 0);
 
 	Line l1(a, b);
 	Line l2(c, d);//
 	
+	//cout << l1.IsIntersecLine(l2) << endl;
+	cout << "Is segments intersection? - "<< l1.IsIntersecSegments(l2) << endl;
+
 	//Point r = l1.IsIntersecLine(l2);
 
 	return 0;
